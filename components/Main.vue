05760a1dc6fc41874917cda8cac442e69e16ed4e
Marcin Dawidziuk <cinekdawidziuk@gmail.com>
2021-12-08 23:34:44 +0000

    Cleanup

--- components/Main.vue
+++ components/Main.vue
@@ -9,7 +9,7 @@
                   src="mypic.png"/>
       </div>
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
-        <h2 class="text-2xl leading-7 font-semibold">
+        <h2 class="text-xl leading-7 font-semibold">
           Hello
         </h2>
         <p class="mt-2">
@@ -17,8 +17,6 @@
         </p>
         <p class="mt-2 text-gray-600">
           I am a Polish software developer living in South Wales, UK.
-          I work mostly in .NET (ASP, Entity Framework and WPF), but always eager to learn new technologies.
-          Dipping my toes in native Android development and Web Dev with Vue.
         </p>
         <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
           You can check out my github page <a class="underline" href="https://github.com/marcindawidziuk">here</a>
@@ -38,6 +36,12 @@
           preserveAspectRatio="xMidYMid meet"
           viewBox="0 0 24 24"
         ><path d="M12 2.247a10 10 0 0 0-3.162 19.487c.5.088.687-.212.687-.475c0-.237-.012-1.025-.012-1.862c-2.513.462-3.163-.613-3.363-1.175a3.636 3.636 0 0 0-1.025-1.413c-.35-.187-.85-.65-.013-.662a2.001 2.001 0 0 1 1.538 1.025a2.137 2.137 0 0 0 2.912.825a2.104 2.104 0 0 1 .638-1.338c-2.225-.25-4.55-1.112-4.55-4.937a3.892 3.892 0 0 1 1.025-2.688a3.594 3.594 0 0 1 .1-2.65s.837-.262 2.75 1.025a9.427 9.427 0 0 1 5 0c1.912-1.3 2.75-1.025 2.75-1.025a3.593 3.593 0 0 1 .1 2.65a3.869 3.869 0 0 1 1.025 2.688c0 3.837-2.338 4.687-4.563 4.937a2.368 2.368 0 0 1 .675 1.85c0 1.338-.012 2.413-.012 2.75c0 .263.187.575.687.475A10.005 10.005 0 0 0 12 2.247z" fill="currentColor" /></svg></a>
+
+        <a href="https://www.linkedin.com/in/marcin-dawidziuk-3b0448185/" target="_blank">
+          <svg role="img"
+               class="w-6 h-6 text-gray-500 hover:text-gray-800"
+               viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>LinkedIn</title><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"  fill="currentColor" /></svg>
+        </a>
       </div>
     </div>
   </div>
